30a5322e1224c48e4a150e8688eb897bb9e274b8
Manish Singh (1000129514) <SINGM66@student.op.ac.nz>
2024-10-17 15:51:26 +1300

    ADD THE CODE

--- MCNR/Program.cs
+++ MCNR/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace MCNR
+﻿using System.Transactions;
+
+namespace MCNR
 {
     internal class Program
     {
+        static void NPC()
+        {
+            int choose;
+            Console.WriteLine("Press 1 for cave or Press 2 for inventory:  ");
+            int decision  = Console.ReadLine();
+            if (decision == 1)
+            {
+
+            }
+             
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
         }
+        
     }
+        
 }
