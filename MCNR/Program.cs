ca39ede85a15d94154ec151ebde1b2f68145ea2e
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2024-10-17 16:05:32 +1300

    added introduction and tutorial control

--- MCNR/Program.cs
+++ MCNR/Program.cs
@@ -19,19 +19,42 @@ namespace MCNR
         }
 
 
-        
+            static void Introduction()
+            {
+                Console.WriteLine("In the peaceful realm of Eldoria, darkness has begun to creep into the lands, threatening the harmony of its inhabitants. <enter>");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("As a young hero named [Enter Name], you awaken to the call of adventure, armed only with your determination and a rusty sword. <enter>");
+                string playersName = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Your journey begins at the edge of your village, where whispers of a powerful foe—the Shadow Lord—stir fear in every heart. <enter>");
+            }
 
-        static void Main(string[] args)
-        {
-            
+            static void TutorialControl()
+            {
+                Console.WriteLine("You step into the world, ready to prove your worth. <enter>");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("A training dummy awaits, your first test. You must learn the art of combat by pressing 'A' to attack.");
+                char attack = Convert.ToChar(Console.ReadLine());
+
+            int enemy1;
+
+
+
+
+
 
         }
 
-        
 
 
-      
-
+            static void Main(string[] args)
+            {
+                Introduction();
+                Console.ReadLine();
+            }
+        }  
     }
         
-}
+
